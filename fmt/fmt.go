5743d1911065167cc4e2638cdc2699a7bce9ae0f
Daniel Hess <dan9186@gmail.com>
2020-02-24 02:01:01 -0800

    some linting

--- fmt/fmt.go
+++ fmt/fmt.go
@@ -6,10 +6,13 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Printf handles all general log messages for commands
 func Printf(f string, args ...interface{}) {
 	fmt.Println(fmt.Sprintf(f, args...))
 }
 
+// Verbosef handles all log messages noted as verbose and does not show unless
+// the verbose flag has been provided to the command
 func Verbosef(f string, args ...interface{}) {
 	if viper.GetBool("verbose") {
 		fmt.Println(fmt.Sprintf(f, args...))
