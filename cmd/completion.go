715f5ffb7a906e2a6aa272a40a773d11dd9f4d84
Daniel Hess <dan9186@gmail.com>
2020-05-14 13:41:57 -0700

    adding completion command

---
+++ cmd/completion.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	defaultShell = "zsh"
+)
+
+var (
+	shell string
+)
+
+func init() {
+	RootCmd.AddCommand(CompletionCmd)
+
+	CompletionCmd.Flags().StringVar(&shell, "shell", defaultShell, "desired shell to generate completions for")
+}
+
+// CompletionCmd represents the command for generating completion files for the
+// flow cli.
+var CompletionCmd = &cobra.Command{
+	Use:   "completion",
+	Short: "Generate completion files for the flow cli",
+	Run:   completionFunc,
+}
+
+func completionFunc(cmd *cobra.Command, args []string) {
+	switch strings.ToLower(shell) {
+	case "bash":
+		RootCmd.GenBashCompletion(os.Stdout)
+	case "fish":
+		RootCmd.GenFishCompletion(os.Stdout, false)
+	case "ps", "powershell", "power_shell":
+		RootCmd.GenPowerShellCompletion(os.Stdout)
+	case "zsh":
+		RootCmd.GenZshCompletion(os.Stdout)
+	default:
+	}
+}
