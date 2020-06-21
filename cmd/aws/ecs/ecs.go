98d486520fbfc2abe00115f93e83f09920d78458
Daniel Hess <dan9186@gmail.com>
2020-06-20 23:53:33 -0700

    allow passing one off envs too

--- cmd/aws/ecs/ecs.go
+++ cmd/aws/ecs/ecs.go
@@ -19,6 +19,7 @@ var (
 
 	cpu     int64
 	envFile string
+	env     []string
 	memory  int64
 	name    string
 )
@@ -27,6 +28,7 @@ func init() {
 	EcsCmd.PersistentFlags().StringVar(&region, "region", "us-east-1", "aws region to use")
 
 	EcsCmd.PersistentFlags().Int64Var(&cpu, "cpu", int64(0), "cpus to assign to the task definition")
+	EcsCmd.PersistentFlags().StringSliceVarP(&env, "env", "e", []string{}, "file to parse env vars from")
 	EcsCmd.PersistentFlags().StringVar(&envFile, "envFile", "", "file to parse env vars from")
 	EcsCmd.PersistentFlags().Int64Var(&memory, "memory", int64(0), "memory to assign to the task definition")
 	EcsCmd.PersistentFlags().StringVar(&name, "name", "", "name to give the task definition")
