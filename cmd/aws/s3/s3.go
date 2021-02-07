f7761d564975ce561141e70ea3269b247fb417c1
Daniel Hess <dan9186@gmail.com>
2021-02-07 00:04:46 -0800

    add basis of s3 cmd

---
+++ cmd/aws/s3/s3.go
@@ -0,0 +1,46 @@
+package s3
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/spf13/cobra"
+
+	"github.com/gomicro/flow/fmt"
+)
+
+var (
+	s3Svc      *s3.S3
+	s3Uploader *s3manager.Uploader
+
+	region string
+)
+
+// S3Cmd represents the root of the s3 command
+var S3Cmd = &cobra.Command{
+	Use:              "s3",
+	Short:            "S3 related commands",
+	PersistentPreRun: configClient,
+}
+
+func configClient(cmd *cobra.Command, args []string) {
+	httpClient := &http.Client{}
+
+	cnf := &aws.Config{
+		Region:     &region,
+		HTTPClient: httpClient,
+	}
+
+	sess, err := session.NewSession(cnf)
+	if err != nil {
+		fmt.Printf("Error creating session: %v", err.Error())
+		os.Exit(1)
+	}
+
+	s3Svc = s3.New(sess)
+	s3Uploader = s3manager.NewUploader(sess)
+}
