af27f19d2fe5cf3ed27fe87d5f0a92125172ad40
Daniel Hess <dan9186@gmail.com>
2022-05-27 17:08:12 -0700

    add list secrets command

--- cmd/aws/aws.go
+++ cmd/aws/aws.go
@@ -3,12 +3,14 @@ package aws
 import (
 	"github.com/spf13/cobra"
 
+	"github.com/gomicro/flow/cmd/aws/asm"
 	"github.com/gomicro/flow/cmd/aws/ecr"
 	"github.com/gomicro/flow/cmd/aws/ecs"
 	"github.com/gomicro/flow/cmd/aws/s3"
 )
 
 func init() {
+	AwsCmd.AddCommand(asm.AsmCmd)
 	AwsCmd.AddCommand(ecr.EcrCmd)
 	AwsCmd.AddCommand(ecs.EcsCmd)
 	AwsCmd.AddCommand(s3.S3Cmd)
