3e8e8f1a7aab0c82eb869de80fd7688483af0c38
Daniel Hess <dan9186@gmail.com>
2022-05-27 23:56:49 -0700

    allow minimal indexing into json objects

--- cmd/aws/asm/get.go
+++ cmd/aws/asm/get.go
@@ -1,16 +1,16 @@
 package asm
 
 import (
+	"encoding/json"
 	"os"
 	"sort"
+	"strings"
 
 	aws_asm "github.com/aws/aws-sdk-go/service/secretsmanager"
 	"github.com/spf13/cobra"
 
 	"github.com/gomicro/flow/fmt"
 )
-
-var ()
 
 func init() {
 	AsmCmd.AddCommand(GetCmd)
@@ -20,7 +20,7 @@ func init() {
 var GetCmd = &cobra.Command{
 	Use:               "get <secret_name>",
 	Short:             "Get secret",
-	Long:              `Get secret from the Secrets Manager`,
+	Long:              `Get secret from the Secrets Manager. Uses of periods in the secret name will be used to denote selecting into a json object if it is returned.`,
 	Args:              cobra.ExactArgs(1),
 	ValidArgsFunction: getCmdValidArgsFunc,
 	Run:               getFunc,
@@ -29,8 +29,14 @@ var GetCmd = &cobra.Command{
 func getFunc(cmd *cobra.Command, args []string) {
 	name := args[0]
 
-	input := &aws_asm.GetSecretValueInput{
-		SecretId: &name,
+	parts := strings.Split(name, ".")
+
+	input := &aws_asm.GetSecretValueInput{}
+
+	if len(parts) > 1 {
+		input.SecretId = &parts[0]
+	} else {
+		input.SecretId = &name
 	}
 
 	resp, err := asmSvc.GetSecretValue(input)
@@ -39,7 +45,25 @@ func getFunc(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 
-	fmt.Printf("%v", *resp.SecretString)
+	if len(parts) > 1 {
+		var heap map[string]interface{}
+		err = json.Unmarshal([]byte(*resp.SecretString), &heap)
+		if err != nil {
+			fmt.Printf("Error unmarshalling secret json: %v", err.Error())
+			os.Exit(1)
+		}
+
+		val, err := getJsonValue(heap, parts[1:])
+		if err != nil {
+			fmt.Printf("Error getting json value: %v", err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Printf("%v", val)
+
+	} else {
+		fmt.Printf("%v", *resp.SecretString)
+	}
 }
 
 func getCmdValidArgsFunc(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
